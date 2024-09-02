021ceae95230e095d16e1494d0bf1014364d989a
Diego Arturo Yangua Merino <103982003+Dacyz@users.noreply.github.com>
2024-09-01 22:30:03 -0500

    fix: wrong main example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,120 +6,105 @@ import 'package:widget_adaptive/widgets/adaptive_stateless.dart';
 import 'package:widget_adaptive/widgets/adaptive_widget.dart';
 import 'package:widget_adaptive/widgets/adaptive_wrap.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Widget Adaptive',
-      color: Colors.black,
-      builder: (context, child) => child!,
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: routes,
-    );
-  }
-}
-
+///                         ⬇ Here any of our Adaptive Widgets
 class SelectorPage extends AdaptiveWrapWidget {
   const SelectorPage({super.key});
 
   @override
+  // [build] method wrap all our defined builders
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(32),
-          children: [
-            ElevatedButton(
-              onPressed: () => _goToWidgetAdaptive(context),
-              child: const Text('Widget Adaptive'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => _goToWidgetWrap(context),
-              child: const Text('Widget Adaptive Wrap'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => _goToWidgetStateless(context),
-              child: const Text('Widget Stateless'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => _goToWidgetStateful(context),
-              child: const Text('Widget Stateful'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => _goToWidgetColorful(context),
-              child: const Text('Widget Colorful'),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Adaptive Display Widgets'),
       ),
+      body: Center(
+          /// ⬇ Here our [adaptive] Widget
+          child: adaptive),
     );
   }
 
-  @override
-  Widget md(BuildContext context, BoxConstraints constraints) {
-    return Scaffold(
-      body: Center(
-        child: Row(
-          children: [
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(32),
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _goToWidgetAdaptive(context),
-                    child: const Text('Widget Adaptive'),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () => _goToWidgetWrap(context),
-                    child: const Text('Widget Adaptive Wrap'),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () => _goToWidgetStateless(context),
-                    child: const Text('Widget Stateless'),
-                  ),
-                  const SizedBox(height: 16),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(32),
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _goToWidgetStateful(context),
-                    child: const Text('Widget Stateful'),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () => _goToWidgetColorful(context),
-                    child: const Text('Widget Colorful'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
+  /// On [AdaptiveWrapWidget] xs become mandatory
   @override
   Widget xs(BuildContext context, BoxConstraints constraints) {
-    // TODO: implement xs
-    throw UnimplementedError();
+    return ListView(
+      padding: const EdgeInsets.all(32),
+      children: [
+        ElevatedButton(
+          onPressed: () => _goToWidgetAdaptive(context),
+          child: const Text('Widget Adaptive'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => _goToWidgetWrap(context),
+          child: const Text('Widget Adaptive Wrap'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => _goToWidgetStateless(context),
+          child: const Text('Widget Stateless'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => _goToWidgetStateful(context),
+          child: const Text('Widget Stateful'),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => _goToWidgetColorful(context),
+          child: const Text('Widget Colorful'),
+        ),
+      ],
+    );
+  }
+
+  // The Builders from sm onwards are all optional 
+  // and if they are not defined they will take 
+  // the value of their predecessor builder
+  @override
+  Widget md(BuildContext context, BoxConstraints constraints) {
+    return Row(
+      children: [
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(32),
+            children: [
+              ElevatedButton(
+                onPressed: () => _goToWidgetAdaptive(context),
+                child: const Text('Widget Adaptive'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => _goToWidgetWrap(context),
+                child: const Text('Widget Adaptive Wrap'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => _goToWidgetStateless(context),
+                child: const Text('Widget Stateless'),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(32),
+            children: [
+              ElevatedButton(
+                onPressed: () => _goToWidgetStateful(context),
+                child: const Text('Widget Stateful'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => _goToWidgetColorful(context),
+                child: const Text('Widget Colorful'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
@@ -151,3 +136,21 @@ final routes = {
   '/widget_stateful': (context) => const StatefulAdaptive(),
   '/widget_colorful': (context) => const ColorfulAdaptive(),
 };
+
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Widget Adaptive',
+      color: Colors.black,
+      builder: (context, child) => child!,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: routes,
+    );
+  }
+}
